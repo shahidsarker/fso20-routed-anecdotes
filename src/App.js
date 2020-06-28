7dcc2d70a9f3314ddc35d1e78edea18cfaf337d1
Shahid <shahid.sarker@gmail.com>
2020-06-28 18:00:04 -0400

    useField custom hook

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   useHistory,
 } from "react-router-dom";
 import "./App.css";
+import { useField } from "./hooks";
 
 const Menu = () => {
   const padding = {
@@ -74,9 +75,9 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -94,27 +95,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
